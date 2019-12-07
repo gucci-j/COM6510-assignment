f3d6544f39bd62c0459118f688a4959c98f6793a
Gucci <30075338+gucci-j@users.noreply.github.com>
2019-12-07 18:24:59 +0000

    Replace EasyImage with defualt functions to extract photo ids

--- app/src/main/java/oak/shef/ac/uk/livedata/MyView.java
+++ app/src/main/java/oak/shef/ac/uk/livedata/MyView.java
@@ -10,25 +10,39 @@ import android.app.Activity;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import oak.shef.ac.uk.livedata.database.NumberData;
 import pl.aprilapps.easyphotopicker.DefaultCallback;
@@ -43,6 +57,11 @@ public class MyView extends AppCompatActivity {
     static private Context context;
     private static final int REQUEST_READ_EXTERNAL_STORAGE = 2987;
     private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 7829;
+
+    private final static int RESULT_CAMERA = 1001;
+    private final static int REQUEST_PERMISSION = 1002;
+    private Uri cameraURI;
+    private File cameraFILE;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,18 +96,22 @@ public class MyView extends AppCompatActivity {
         });
 
 
-        // for camera
+        // for taking a photo
+        initEasyImage();
         if (checkCameraHardware(getApplicationContext()) == true) {
-            Log.i("Debug", "onCreate: Has a camera");
-
-            checkPermissions(getApplicationContext()); // required by Android 6.0 +
-            initEasyImage();
+            // A camera button will be visible
             FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_camera);
             myViewModel.setCameraButton(fab);
+
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    EasyImage.openCamera(getActivity(), 0);
+                    if (Build.VERSION.SDK_INT >= 23) {
+                        checkPermission();
+                    }
+                    else {
+                        cameraIntent();
+                    }
                 }
             });
         }
@@ -119,6 +142,7 @@ public class MyView extends AppCompatActivity {
         });
     }
 
+
     public Activity getActivity() {
         return activity;
     }
@@ -131,65 +155,132 @@ public class MyView extends AppCompatActivity {
                 .setAllowMultiplePickInGallery(true);
     }
 
-    /** Check if this device has a camera */
-    /** ref: https://developer.android.com/guide/topics/media/camera */
+
+    /**
+     * Check camera hardware
+     * Desc: This function checks whether a device has a camera or not.
+     * Ref: https://developer.android.com/guide/topics/media/camera
+     *      https://developer.android.com/training/camera/photobasics#java
+     * @param context
+     * @return true or false
+     */
     private boolean checkCameraHardware(final Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
             // this device has a camera
+            Log.i("debug", "checkCameraHardware: Has a camera");
             return true;
         } else {
             // no camera on this device
+            Log.i("debug", "checkCameraHardware: No camera");
             return false;
         }
     }
 
-    private void checkPermissions(final Context context) {
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(context);
-                    alertBuilder.setCancelable(true);
-                    alertBuilder.setTitle("Permission necessary");
-                    alertBuilder.setMessage("External storage permission is necessary");
-                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
-                        }
-                    });
-                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
-                    alert.show();
 
-                } else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
-                }
-            }
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(context);
-                    alertBuilder.setCancelable(true);
-                    alertBuilder.setTitle("Permission necessary");
-                    alertBuilder.setMessage("Writing external storage permission is necessary");
-                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
-                        }
-                    });
-                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
-                    alert.show();
+    /**
+     * Check Permission
+     * Desc: This function is for checking permission to get photos.
+     * Ref: https://developer.android.com/guide/topics/permissions/overview
+     */
+    private void checkPermission(){
+        Log.d("debug","checkPermission()");
+        // If already got permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            cameraIntent();
+        }
+        // If permission is denied
+        else{
+            requestPermission();
+        }
+    }
 
-                } else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
-                }
+
+    /**
+     * Request permission
+     * Desc: This function requests permissions for storing images in external storage.
+     * Ref: https://developer.android.com/reference/android/support/v4/app/ActivityCompat.html#shouldShowRequestPermissionRationale(android.app.Activity,%20java.lang.String)
+     */
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(
+                    MyView.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        } else {
+            Toast toast = Toast.makeText(
+                    this,
+                    "Need permission for saving images!",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    REQUEST_PERMISSION);
+        }
+    }
+
+
+    /**
+     * onRequestPermissionsResult
+     * Desc: This function deals with the camera permission.
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d("debug","onRequestPermissionsResult()");
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraIntent();
+
+            } else {
+                Toast toast = Toast.makeText(this,
+                        "Need permission for saving images!", Toast.LENGTH_SHORT);
+                toast.show();
             }
         }
     }
 
+
+    /**
+     * Camera Intent
+     * Desc:
+     * Ref: https://stackoverflow.com/questions/42992989/storing-image-resource-id-in-sqlite-database-and-retrieving-it-in-int-array
+     *      https://developer.android.com/training/camera/photobasics#TaskPath
+     */
+    private void cameraIntent(){
+        // designate an external storage folder
+        File saveFolder = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        // obtain a timestamp for a photo
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        // define file name
+        String fileName = String.format("COM6510_%s.jpg", timeStamp);
+
+        cameraFILE = new File(saveFolder, fileName);
+        cameraURI = FileProvider.getUriForFile(
+                MyView.this,
+                getApplicationContext().getPackageName() + ".fileprovider",
+                cameraFILE);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraURI);
+        startActivityForResult(intent, RESULT_CAMERA);
+    }
+
+
+    /**
+     * onActivityResult
+     * Desc: This function is for adding images to the external storage or uploading images from the gallery.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.d("debug","onActivityResult()");
 
         EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
             @Override
@@ -212,11 +303,35 @@ public class MyView extends AppCompatActivity {
                 }
             }
         });
+
+        if (requestCode == RESULT_CAMERA) {
+            if(cameraURI != null){
+                registerDatabase(cameraFILE);
+            }
+            else{
+                Log.d("debug","cameraURI is null");
+            }
+        }
     }
 
 
     /**
-     * Save to the picturelist
+     * registerDatabase
+     * Desc: This function is for registering photos to the external storage.
+     * @param file
+     */
+    private void registerDatabase(File file) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = MyView.this.getContentResolver();
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put("_data", file.getAbsolutePath());
+        contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues);
+    }
+
+    /**
+     * onPhotosReturned
+     * Desc: save to the picturelist
      * @param returnedPhotos
      */
     private void onPhotosReturned(List<File> returnedPhotos) {
